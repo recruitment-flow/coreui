876cb19a218dcc8406f0fcab2521cb8b0a51bc66
admin <recruitmentflow2023@gmail.com>
2023-10-05 15:23:33 +0530

    changes added

--- src/app/views/requirement/requirement-list/requirement-list.component.ts
+++ src/app/views/requirement/requirement-list/requirement-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-requirement-list',
   templateUrl: './requirement-list.component.html',
   styleUrls: ['./requirement-list.component.scss']
 })
-export class RequirementListComponent {
+export class RequirementListComponent implements OnInit {
+  requirementForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.requirementForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: [''],
+      description1: [''],
+    });
+  }
+  onSubmit(){
+
+  }
+  // Implement form submission logic here
 }
--- src/app/views/requirement/requirement-list/requirement-list.module.ts
+++ src/app/views/requirement/requirement-list/requirement-list.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RequirementListRoutingModule } from './requirement-list-routing.module';
 import { RequirementListComponent } from './requirement-list.component';
-
+import { SharedModule } from '../../../modules/shared.module'
 @NgModule({
   declarations: [
     RequirementListComponent
   ],
   imports: [
     CommonModule,
-    RequirementListRoutingModule
+    RequirementListRoutingModule,
+    SharedModule
   ]
 })
 export class RequirementListModule { }
